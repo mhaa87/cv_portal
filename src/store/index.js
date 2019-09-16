104da57bc42dc3eedbbbb8611f27334299fcab65
mhaa87 <47102878+mhaa87@users.noreply.github.com>
2019-09-16 10:46:21 +0200

    Add files via upload

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import Axios from 'axios'
 import Fonts from'./fonts.json'
-import CVobj from './cvContent.json'
+import cvContentEng from './cvContent.json'
+import cvContentNor from './cvContentNorsk.json'
 
 Vue.use(Vuex)
 Axios.defaults.headers.post["Content-Type"] = "application/json";
@@ -17,9 +18,10 @@ export default new Vuex.Store({
         page: "home",
         cvList: [],
         showCVmenu: false,
+        isSaving: false,
         showEditWindow: false,
-        editWindow: {show: false, info: {}},
-        content: CVobj,
+        editWindow: {show: false, type: '', data: {}},
+        content: cvContentEng,
         fonts: Fonts,
         user: {
             name: "tester",
@@ -39,8 +41,8 @@ export default new Vuex.Store({
 
     mutations: {
         setPage: (store, newPage) => store.page = newPage,
-        setLoginStatus: (store, status) => {store.loggedIn = status},
-        setAutoLogin: (store, value) => {store.autoLogin = value},
+        setLoginStatus: (store, status) => store.loggedIn = status,
+        setAutoLogin: (store, value) => store.autoLogin = value,
         setKey: (store, key) => store.key = key,
         setStyle: (store, style) => store.content.style = style,
         setFont: (store, font) => store.content.style.font = font,
@@ -48,24 +50,40 @@ export default new Vuex.Store({
         setImg: (store, link) => store.content.style.imgLink = link,
         setImgSize: (store, size) => store.content.style.imgSize = size,
         setEditMode: (store, value) => store.editMode = value,
-        setEditWindow: (store, info) => store.editWindow = info,
+        setEditWindow: (store, info) => {
+            if(store.editWindow.show && info.show) return;
+            store.editWindow = {'show': info.show, 'type': info.type, 'data': info.data}
+        },
+        setCV(store, cv) {store.content = cv},
         sendCVcontent: (store, func) => store.getCVcontent = func,
+        setDefaultCV: (store, lang) => {
+            if(lang === "norsk") store.content = cvContentNor;
+            else store.content = cvContentEng;
+        },
         toggleLogin: (store, register) => {
             store.showLogin = register ? (!store.register || !store.showLogin) : (!store.showLogin || store.register);
             store.register = register;
             store.error = false;
         },
-        toggleCVmenu: (store) => {store.showCVmenu = !store.showCVmenu},
+        setCVmenu: (store, show) => {store.showCVmenu = show},
+        mainContentSwap: (store, index) => {
+            var list = store.content.mainContent;
+            if(index.j < 0 || (index.j-1) > list.length) return;
+            var temp = list[index.i];
+            Vue.set(store.content.mainContent, index.i, list[index.j]);
+            Vue.set(store.content.mainContent, index.j, temp);
+        }
     },
 
     actions: {  
         download(context){
             if(context.state.editMode) return;
             var cv = document.getElementById("print");
-            var popupWin = window.open('', '_blank', 'width= 1000px,height=900,location=no,left=200px, top=20px');
+            var popupWin = window.open('', '_blank', 'width= 700px,height=900,location=no,left=200px, top=20px');
             popupWin.document.open();
-            popupWin.document.write('<html><head><title>Preview</title><link rel="stylesheet" type="text/css" href="printStyle.css" /></head><body onload="window.print()">'
-            + '<div class="cvPrint">'); 
+            popupWin.document.write(
+                '<html><head><title>Preview</title><link rel="stylesheet" type="text/css" href="printStyle.css" /></head>'
+                +'<body onload="window.print()"> <div class="cvPrint">'); 
             popupWin.document.write(cv.innerHTML);
             popupWin.document.write('</div></body></html>');
             popupWin.document.close();
@@ -88,6 +106,7 @@ export default new Vuex.Store({
             localStorage.setItem("key", data.key);
             localStorage.setItem("autoLogin", store.autoLogin ? 'true' : 'false');
             store.user.name = data.name;
+            context.state.cvList = data.cvList;
             // this.getLastCV();
         },
 
@@ -99,21 +118,20 @@ export default new Vuex.Store({
             context.state.user = {name: "", email: "", password: ""};
         },
 
+        newComment: async (context, comment) => {
+            await Axios.post(context.state.uri + "/addComment", {'comment': comment});
+        },
+
         saveAction: async (context, name) => {
             context.state.content.cvName = name;
-            await Axios.post(context.state.uri + "/saveCV", {"key": context.state.key, "content": context.state.content});
+            context.state.isSaving = true;
+            var res = await Axios.post(context.state.uri + "/saveCV", {"key": context.state.key, "content": context.state.content});
+            context.state.cvList = res.data;
+            setTimeout(() => context.state.isSaving = false, 600)
         },
 
-        newCV: async (context, name) => {
-            context.state.content = CVobj;
-            context.state.content.cvName = name;
-            await Axios.post(context.state.uri + "/saveCV", {"key": context.state.key, "content": context.state.content});
-        },
-
-        loadCVlist: async(context, showMenu) => {
-            if(!showMenu){context.state.showCVmenu = false; return;}
-            context.state.showCVmenu = true;
-            var res = await Axios.post(context.state.uri + "/cvMenu", {"key": context.state.key})
+        loadCVlist: async(context) => {
+            var res = await Axios.post(context.state.uri + "/cvMenu", {"key": context.state.key});
             context.state.cvList = res.data.status ? res.data.cvList : [];
         },
 
@@ -123,8 +141,8 @@ export default new Vuex.Store({
         },
 
         deleteCV: async(context) => {
-            await Axios.post(context.state.uri + "/delete", {"key": context.state.key, "cvName": context.state.content.cvName});
-            // await context.state.loadCVlist(true);
+            var res = await Axios.post(context.state.uri + "/delete", {"key": context.state.key, "cvName": context.state.content.cvName});
+            context.state.cvList = res.data;
         },
 
         deleteAll: async(context) => {
